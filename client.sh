12c11b7985bd70a1e1e315ba3e1db47798947c40
AlejoTHR <alejandrodavid.pereira@estudiant.enti.cat>
2026-02-13 13:12:28 +0100

    Nuevo Cliente, Problemas con la conección

--- client.sh
+++ client.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-SERVER_IP="192.168.1.57"
-PORT=64113
+SERVER_IP="10.65.0.41"
+PORT=60000
 
-echo "HELLO" | nc -q 0 $SERVER_IP $PORT
+echo "Conectant..."
+
+echo "HELLO" | nc -q 3 $SERVER_IP $PORT
+
+# Era que no habia tiempo para recibir un mensaje
 response=$(nc -l -p $PORT)
 
+echo "DEBUG"
+
 if [[ "$response" != "OK" ]]; then
+  echo "Conexio Rebutjada"
   exit 1
 fi
 
 while true; do
-
-  echo "Esperant el torn ..."
+  echo "Esperand Torn ..."
 
   # Sacaba el temps despera quan el servidor acaba el torn
   response=$(nc -l -p $PORT)
