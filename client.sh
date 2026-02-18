defea3722a20d7b90f700494f48638e5cc5a1870
AlejoTHR <alejandrodavid.pereira@estudiant.enti.cat>
2026-02-18 15:26:06 +0100

    Client.sh Revisión (np fnciona)

--- client.sh
+++ client.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-SERVER_IP="10.65.0.41"
-PORT=60000
+SERVER_IP="10.65.0.44"
+PORT=50000
+BOARD=(1 2 3 4 5 6 7 8 9)
 
-echo "Conectant..."
 
-echo "HELLO" | nc -q 3 $SERVER_IP $PORT
+echo "Trobant Conexio"
 
-# Era que no habia tiempo para recibir un mensaje
+echo "HELLO" | nc "10.65.0.44" "50000"
+
+echo "HELLO enviat"
+
+#echo "HELLO" | nc $SERVER_IP $PORT
+
 response=$(nc -l -p $PORT)
-
-echo "DEBUG"
 
 if [[ "$response" != "OK" ]]; then
   echo "Conexio Rebutjada"
   exit 1
+fi
+
+
+if[[ "$response" == "OK" ]]; then
+
+echo "Conectat"
 fi
 
 while true; do
@@ -24,6 +33,11 @@ while true; do
   response=$(nc -l -p $PORT)
 
   # TODO: Gestió de missatges rebuts
+  
+
+  ## BOARD
+  #print_board()
+
   # SERVER_WIN
   # CLIENT_WIN
   # MOVE_CLIENT
@@ -31,7 +45,7 @@ while true; do
 
   # == TORN CLIENT ==
   # TODO: pregunta posició i s'envia al servidor
-  echo "MOVE $pos" | nc -q 0 $SERVER_IP $PORT
+  #echo "MOVE $pos" | nc -q 0 $SERVER_IP $PORT
 
 done
 
